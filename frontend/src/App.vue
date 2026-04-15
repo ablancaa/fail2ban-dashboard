db5929ad220d28a139618d3a8546e483bda8b5eb
ablancaa <ablancaa@gmail.com>
2026-04-15 20:23:17 +0200

    clock

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -58,7 +58,7 @@
         <!-- Total de IPs baneadas: {{ totalBanned }} -->
          <!-- ⏰ reloj -->
             <!-- ⏰ reloj -->
- <div class="flex items-center gap-2 bg-white dark:bg-slate-800 px-3 py-1 rounded-full shadow text-black dark:text-black font-mono text-sm">
+ <div class="flex items-center gap-2 bg-white dark:bg-slate-800 px-3 py-1 rounded-full shadow text-black dark:text-white font-mono text-sm">
   <span class="animate-pulse">🕒</span>
   <span>{{ clock }}</span>
 </div>
@@ -167,11 +167,7 @@ onMounted(() => {
   setInterval(fetchServiceStatus, 5000)
   updateUptimeChart()
   updateClock()
-  clock.value = new Date().toLocaleString()
-
- timer = setInterval(() => {
-  updateClock()
-}, 1000)
+  timer = setInterval(updateClock, 1000)
 })
 
 onUnmounted(() => {

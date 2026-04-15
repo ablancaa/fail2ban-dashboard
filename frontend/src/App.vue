0bbda6d8d964f5357875082f08ec0a2dace78505
ablancaa <ablancaa@gmail.com>
2026-04-15 20:24:10 +0200

    clock

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -59,7 +59,7 @@
          <!-- ⏰ reloj -->
             <!-- ⏰ reloj -->
  <div class="flex items-center gap-2 bg-white dark:bg-slate-800 px-3 py-1 rounded-full shadow text-black dark:text-white font-mono text-sm">
-  <span class="animate-pulse">🕒</span>
+  <span class="animate-pulse">🕒 {{ clock }}</span>
   <span>{{ clock }}</span>
 </div>
       </div>

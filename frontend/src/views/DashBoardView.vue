0358afb4355fb1db001e099fb61e67fb6de69111
ablancaa <ablancaa@gmail.com>
2026-04-30 13:15:24 +0200

    Mejoras en css, retiro bloques del final

--- frontend/src/views/DashBoardView.vue
+++ frontend/src/views/DashBoardView.vue
@@ -167,21 +167,20 @@
       </div>
     </div>
 
-    <br />
     <!-- ==================== COMPONENTES ADICIONALES ==================== -->
     <!-- Grid de 2 columnas: Logs y Configuración -->
-    <div class="grid grid-cols-1 gap-2 md:grid-cols-2">
-      <!-- Componente Logs: muestra el historial de logs -->
+    <!-- <div class="grid grid-cols-1 gap-2 md:grid-cols-2"> -->
+    <!-- Componente Logs: muestra el historial de logs -->
 
-      <div class="bg-white rounded-xs shadow p-4">
-        <!-- <LogsHistory /> -->
-      </div>
+    <!-- <div class="bg-white rounded-xs shadow p-4"> -->
+    <!-- <LogsHistory /> -->
+    <!-- </div> -->
 
-      <!-- Componente JailConfig: muestra la configuración de los jails -->
-      <div class="bg-white rounded-xs shadow p-4">
-        <!-- <JailConfig /> -->
-      </div>
-    </div>
+    <!-- Componente JailConfig: muestra la configuración de los jails -->
+    <!-- <div class="bg-white rounded-xs shadow p-4"> -->
+    <!-- <JailConfig /> -->
+    <!-- </div> -->
+    <!-- </div> -->
   </div>
 </template>
 

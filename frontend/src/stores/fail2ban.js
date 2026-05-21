898901f4e1ecc390ec10b1946530667762372044
ablancaa <ablancaa@gmail.com>
2026-05-21 19:44:33 +0200

    historico en store

--- frontend/src/stores/fail2ban.js
+++ frontend/src/stores/fail2ban.js
@@ -7,35 +7,51 @@ export const useFail2BanStore = defineStore("fail2ban", () => {
   let socket;
 
   const servers = ref([]);
-
-
-
+  const bans = ref([]);
+  
   function connectSocket() {
     if (socket) return;
 
     socket = io("http://192.168.1.137:3000");
 
-    socket.on("status", data => {
+    socket.on("status", (data) => {
       servers.value = data || [];
     });
+
+    // 🔥 HISTÓRICO EN TIEMPO REAL
+    socket.on("bans-history", (data) => {
+      bans.value = data || [];
+    });
   }
-
-
 
   function refresh() {
     socket?.emit("refresh");
   }
 
+   async function fetchBans() {
+    try {
+      const res = await axios.get(
+        "http://192.168.1.137:3000/api/bans-history"
+      );
+
+      bans.value = res.data || [];
+    } catch (err) {
+      console.error("Error cargando bans:", err);
+    }
+  }
+
   async function control(serverId, action) {
     await axios.post(`http://192.168.1.137:3000/api/fail2ban/${action}`, {
-      serverId
+      serverId,
     });
   }
 
   return {
     servers,
+    bans,
     connectSocket,
     refresh,
-    control
+    fetchBans,
+    control,
   };
 });
